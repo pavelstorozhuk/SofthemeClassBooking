9202fae4e237c39beddc3b220537d9aa1d52e2ed
pavelstorozhuk <pavelstorozhuk94@gmail.com>
2016-11-16 15:40:27 +0200

    New Project

---
+++ SofthemeClassBooking/SofthemeClassBooking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SofthemeClassBooking.Startup))]
+namespace SofthemeClassBooking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
