1c9f050d8ba59045a538ba91d8c2f365535b5659
ArtemMorg <art.morhun@gmail.com>
2016-11-17 17:28:08 +0200

    Creation of DB project

---
+++ SofthemeClassBooking/SofthemeClassBookingDb/ClassRoom.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ClassRoom]
+(
+	[Id] uniqueidentifier PRIMARY KEY NOT NULL,
+	[Name] nvarchar(30) NOT NULL,
+	[Capacity] tinyint,
+	[PcCount] tinyint,
+	[PrinterCount] tinyint,
+	[ProjectorCount] tinyint,
+	[DeskCount] tinyint,
+	[Status] tinyint NOT NULL
+)
---
+++ SofthemeClassBooking/SofthemeClassBookingDb/Feedback.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Feedback]
+(
+	[Id] uniqueidentifier PRIMARY KEY NOT NULL,
+	[Name] nvarchar(50) NOT NULL,
+	[Text] nvarchar(max) NOT NULL
+)
