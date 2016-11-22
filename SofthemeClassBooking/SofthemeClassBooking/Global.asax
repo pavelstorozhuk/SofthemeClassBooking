d1f2cfbcb0d31cc0ea23ae82a836a5de171487e6
pavelstorozhuk <pavelstorozhuk94@gmail.com>
2016-11-22 17:44:17 +0200

    fix it all

--- SofthemeClassBooking/SofthemeClassBooking/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SofthemeClassBooking.MvcApplication" Language="C#" %>
--- SofthemeClassBooking/SofthemeClassBooking/Scripts/_references.js
+++
@@ -1,5 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="modernizr-2.6.2.js" />
-/// <reference path="respond.min.js" />
-/// <reference path="jquery.validate.js" />
-/// <reference path="jquery.validate.unobtrusive.min.js" />
